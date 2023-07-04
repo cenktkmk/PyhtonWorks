c437c0b0eadf07ff06e7cf39e0fe18025bc608a6
Cenktkmk <116560930+cenktkmk@users.noreply.github.com>
2023-07-04 22:14:19 +0300

    Add files via upload

---
+++ ödev.py/ödev.py
@@ -0,0 +1,74 @@
+
+#ödev 1
+
+
+#sayi1 = 25
+#sayi2 = 30
+
+#sonuc = int(str(sayi1) + str(sayi2))
+#print("sayi1+sayi2 sonucu:", sonuc)
+
+
+
+
+#ödev 2
+
+
+#isim = input("lütfen isim giriniz: ")
+
+#kücükİsim = isim.lower()
+
+
+#print("Hoş geldiniz..", kücükİsim)
+
+
+
+# ödev 3
+
+
+
+#ad = input("lütfen İsim giriniz: ")
+#soyAd = input("Lütfen soy isim giriniz: ")
+#yaş = input("lütfen yaş giriniz: ")
+
+
+#if not yaş.isdigit():
+    #print("Lütfen string değer girmeyiniz")    
+   
+
+#else:
+    #print("Adınız: ", ad, "soyAdınız: ", soyAd, "Yaşınız:", yaş)
+    
+
+
+
+#ödev 4 
+
+
+#sayi1 = int(input("Lütfen Sayi Giriniz: "))
+#sayi2 = int(input("Lütfen Sayi Giriniz: "))
+#sayi3 = int(input("Lütfen Sayi Giriniz: "))
+
+
+#toplamları = sayi1 + sayi2 + sayi3 
+
+
+#print("Sayıların toplamı: ", toplamları)
+
+
+
+
+
+# ödev 5
+
+
+#metin = input("metin giriniz: ")
+
+#kelime1 = input("değiştirmek istediğiniz kelimeyi giriniz: ")
+#kelime2 = input("yeni kelimeyi giriniz: ")
+
+#metin2 = metin.replace(kelime1, kelime2)
+
+#print("Yeni Metin:" , metin2)
+
+
